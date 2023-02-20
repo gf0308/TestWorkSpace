218ed7786ddfe2e2973b8588be22d0c9fe93bbac
jb_kim <jb.kim@quintet.co.kr>
2023-02-20 22:15:53 +0900

    (2023.02.20) 파일업로드 테스트

--- src/main/java/com/work/fileUpload/FileUploadController.java
+++ src/main/java/com/work/fileUpload/FileUploadController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 //@RestController
 
 @Controller
@@ -19,11 +22,29 @@ public class FileUploadController {
         System.out.println("==========================================================");
 
         String result = "";
+//        String saveDestPath = "C:\\kjb\\works\\output_files\\outputfile.txt";
+//        String saveDestPath = "C:\\kjb\\works\\output_files\\sample_text.txt";
         String saveDestPath = "C:\\kjb\\works\\output_files";
+        String finalFullPathName = saveDestPath + File.separator + file.getOriginalFilename(); // File 오브젝트에서 getOriginalFileName() 결과인 originalFileName 자체가 '확장자' 명까지 포함한 문자열값이다.
 
         // file 유효성 확인 : Name, ContentType, Size 맞는지 확인
+        System.out.println("file.getName: "+ file.getName());
+        System.out.println("file.getContentType: "+ file.getContentType());
+        System.out.println("file.getOriginalFilename: "+ file.getOriginalFilename());
 
         // 파일을 목표저장소에 저장
+        FileOutputStream fileOutputStream;
+        byte[] bytes = null;
+        try {
+            fileOutputStream = new FileOutputStream(finalFullPathName);
+            bytes = file.getBytes();
+            fileOutputStream.write(bytes);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("저장완료");
+
 
         // 저장결과에 대해 작성 후 결과 리턴
 
---
+++ src/test/java/com/work/testworkspace/FileIOTest.java
@@ -0,0 +1,27 @@
+package com.work.testworkspace;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileOutputStream;
+
+public class FileIOTest {
+
+    @Test
+    void test() {
+        FileOutputStream fileOutputStream;
+        String saveDestPath = "C:\\kjb\\works\\output_files\\output.txt";
+        String str = "테스트 문자열 입니다.";
+        byte[] bytes = str.getBytes();
+
+        try {
+            fileOutputStream = new FileOutputStream(saveDestPath);
+            fileOutputStream.write(bytes);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+//        fileOutputStream.close();
+
+        System.out.println("쓰기 완료");
+    }
+
+}
