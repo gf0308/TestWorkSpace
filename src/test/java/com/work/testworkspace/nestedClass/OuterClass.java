79d73f5877cbdc2b9eadc7824dcf31a0292241c7
jb_kim <jb.kim@quintet.co.kr>
2023-02-27 15:43:22 +0900

    (2023.02.27) Nested Class 설명 추가

--- src/test/java/com/work/testworkspace/nestedClass/OuterClass.java
+++ src/test/java/com/work/testworkspace/nestedClass/OuterClass.java
@@ -1,14 +1,47 @@
 package com.work.testworkspace.nestedClass;
 
 /**
- * [중첩클래스(Nested Class) 종류]
+ * [ 중첩클래스(Nested Class) 란? ]
+ * : 클래스 안의 클래스를 말한다.
+ *
+ * ex)
+ * public class OuterClass {
+ *     private String name;
+ *     private int age;
+ *
+ *     public OuterClass(String name, int age) {
+ *         this.name = name;
+ *         this.age = age;
+ *     }
+ *
+ *     public void introduce() {
+ *         System.out.println("Hello, I am " + this.name + ", and " + this.age + " years old.");
+ *     }
+ *
+ * 	    // ##Inner Class
+ *     class InnerClass {
+ *         void introduceInInner() {
+ *             System.out.println("[In Inner Class] Hello, I am " + name + ", and " + age + " years old...!");
+ *         }
+ *     }
+ * }
+ *
+ *
+ * [ 중첩클래스(Nested Class)를 사용하는 이유 ]
+ * 1. 클래스들을 논리적으로 그룹핑하는 효과 (서로 아주 긴밀하게 관련이 있고 오로지 한 곳에서만 사용되는 클래스들을 논리적으로 묶어놓는 효과)
+ * 2. 캡슐화를 증대하는 효과
+ * 3. 더욱 가독성이 높고 유지보수성이 높은 코드로 이끄는 효과
+ * 4. 부모-자식 클래스 연결관계를 더욱 단순명료하게(simpler) 하는 효과 (각 클래스의 변수들과 메서드들을 시각적으로 묘사하기에)
+ *
+ *
+ * [ 중첩클래스(Nested Class) 종류 ]
  *
  * #Nested Class : 중첩 클래스
- *  ## Static Nested Class : Static 중첩 클래스
- *  ## Inner Class : 내부 클래스
- *     ### Inner Class : 일반 내부 클래스 (Outer Class 안에 선언되어 있는 클래스[Inner Class])
- *     ### Method Local Inner Class : 한 메서드 내부에서 지역변수처럼 선언되어서 쓰이는 (내부) 클래스
- *     ### Anonymous Inner Class : 익명 내부 클래스; 기존에 존재하는 어떤 클래스를 new 해서 생성하고 중괄호{}를 전개해 바로 오버라이드 해서 만들어 쓰는 내부 클래스; 메서드 안에서 사용됨.
+ *  ㄴ## Static Nested Class : Static 중첩 클래스
+ *  ㄴ## Inner Class : 내부 클래스
+ *     ㄴ### Inner Class : 일반 내부 클래스 (Outer Class 안에 선언되어 있는 클래스[Inner Class])
+ *     ㄴ### Method Local Inner Class : 한 메서드 내부에서 지역변수처럼 선언되어서 쓰이는 (내부) 클래스
+ *     ㄴ### Anonymous Inner Class : 익명 내부 클래스; 기존에 존재하는 어떤 클래스를 new 해서 생성하고 중괄호{}를 전개해 바로 오버라이드 해서 만들어 쓰는 내부 클래스; 메서드 안에서 사용됨.
  *
  * */
 public class OuterClass {
